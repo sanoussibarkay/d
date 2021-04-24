b2557460b16d6f2b7e445e8a96ca083e71c4a57e
sanoussibarkay <barkaysanoussi@yahoo.fr>
2021-04-24 22:35:45 +0800

    first commit

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php echo " this is a php hello world"; ?>
+    <h2>this is a nomal helo</h2>
+
+  </body>
+</html>
